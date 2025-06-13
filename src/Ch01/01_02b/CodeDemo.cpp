2c9e30b58062dafff2957b875abc08a08f77daeb
caldersong <chaebeens@gmail.com>
2025-06-13 19:53:36 +0000

    Made progress in the course

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,17 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream> //library name
+
+/* This program prints the message "Hi There" in the terminal output */
+
+int main(){
+    std::cout << "Hi There!" << std::endl;
+    //std:cout = standard character output? 
+    //std:endl = adds a new line 
+
+    std::cout << std::endl << std::endl;
+    return 0; // represent: no error in the program
+}
+
